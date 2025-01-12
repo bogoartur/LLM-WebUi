efe83eb45eae8fe221b1de3531b36f0c2887f2a3
PasteleiroM <artursantos@univille.br>
2025-01-12 20:16:11 -0300

    Migrating Spring to API to work with React

--- src/main/java/llmwebui/controller/MensagemController.java
+++ src/main/java/llmwebui/controller/MensagemController.java
@@ -1,22 +1,25 @@
 package llmwebui.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 
+import llmwebui.entity.Mensagem;
 import llmwebui.service.MensagemService;
 
 @Controller
-@RequestMapping("/")
+@RequestMapping("/api/mensagem")
 public class MensagemController {
     @Autowired
     private MensagemService service;
 
     @GetMapping
-    public ModelAndView index() {
-        //var listaMensagens = service.getAll();
-        return new ModelAndView("home/index");
+    public ResponseEntity<List<Mensagem>> getAllMensagens() {
+        List<Mensagem> listaMensagens = service.getAll();
+        return ResponseEntity.ok(listaMensagens);
     }
 }
